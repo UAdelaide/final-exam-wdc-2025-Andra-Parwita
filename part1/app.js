1766514f798a7eb57084f99294dfb0c1330e9e14
Andra_Parwita <andra.parwita@gmail.com>
2025-06-20 01:39:49 +0000

    [automatic commit] 2025-06-20 01:39:49 UTC 180.150.112.31

--- part1/app.js
+++ part1/app.js
@@ -108,7 +108,7 @@ let db;
 })();
 
 //apis
-app.get('/api/dogs', async (req,res) => {
+app.get('/api/dogs', async (req, res) => {
   try {
     const [rows] = await pool.query(`
       SELECT d.name AS dog_name, d.size, u.username AS owner_username
