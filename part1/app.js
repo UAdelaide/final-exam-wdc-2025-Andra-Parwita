fec52f26b3395796af67dc57c5fbf85efe0e81e8
Andra_Parwita <andra.parwita@gmail.com>
2025-06-20 01:36:05 +0000

    [automatic commit] 2025-06-20 01:36:05 UTC 180.150.112.31

--- part1/app.js
+++ part1/app.js
@@ -49,16 +49,18 @@ let db;
           password_hash VARCHAR(255) NOT NULL,
           role ENUM('owner', 'walker') NOT NULL,
           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );
+      `);
 
+    await db.execute(`
       CREATE TABLE IF NOT EXISTS Dogs (
           dog_id INT AUTO_INCREMENT PRIMARY KEY,
           owner_id INT NOT NULL,
           name VARCHAR(50) NOT NULL,
           size ENUM('small', 'medium', 'large') NOT NULL,
           FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-      );
+      `);
 
+    await db.execute(`
       CREATE TABLE IF NOT EXISTS WalkRequests (
           request_id INT AUTO_INCREMENT PRIMARY KEY,
           dog_id INT NOT NULL,
