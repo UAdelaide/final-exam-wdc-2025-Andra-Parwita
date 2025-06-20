b70acc56523a79e756870fb82247a1b53e20238a
Andra_Parwita <andra.parwita@gmail.com>
2025-06-20 04:46:25 +0000

    [automatic commit] 2025-06-20 04:46:25 UTC 180.150.112.31

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -48,6 +48,7 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
+    const user = rows
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
