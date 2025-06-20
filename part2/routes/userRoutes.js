4896f37c51b03cab64b1861f910c4690398ca0c5
Andra_Parwita <andra.parwita@gmail.com>
2025-06-20 05:39:50 +0000

    [automatic commit] 2025-06-20 05:39:50 UTC 180.150.112.31

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -58,10 +58,12 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     if (err) {
       return res.status(500).json({ error: 'Could not log out' });
     }
+
+    res.clearCookie
   });
 });
 
