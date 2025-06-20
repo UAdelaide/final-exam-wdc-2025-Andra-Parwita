e1d56b052bc2927c3f0b6fbbbc6e16ffd0d901e5
Andra_Parwita <andra.parwita@gmail.com>
2025-06-20 02:04:17 +0000

    [automatic commit] 2025-06-20 02:04:17 UTC 180.150.112.31

--- part1/app.js
+++ part1/app.js
@@ -111,7 +111,7 @@ let db;
 app.get('/api/dogs', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT d.name AS dog_name,d.size, u.username AS owner_username
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
       FROM Dogs d
       JOIN Users u ON d.owner_id = u.user_id
     `);
