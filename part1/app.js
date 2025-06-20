282e2421b380ded6789cb3c5a74ada404c98a753
Andra_Parwita <andra.parwita@gmail.com>
2025-06-20 01:48:59 +0000

    [automatic commit] 2025-06-20 01:48:59 UTC 180.150.112.31

--- part1/app.js
+++ part1/app.js
@@ -126,6 +126,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
   try {
     const [rows] = await db.query(`
         SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
+        FROM WalkRequests wr
+        
     `);
     res.json(rows);
   } catch (err) {
